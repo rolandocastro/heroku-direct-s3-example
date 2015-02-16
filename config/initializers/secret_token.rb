7c27957a1337ac7b2bd219998ea4712a6f7971ca
rolandocastro <rolandocastro@gmail.com>
2015-02-16 17:00:28 -0600

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HerokuDirectS3Example::Application.config.secret_key_base = 'e62c26b4989f149db999a3ba021801c77b41415c23959225b89a87c9650eb9a96f8ac2d5f437bdd0ec00a55b516aa319709c84e1a17714871ca5f9be7e638111'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HerokuDirectS3Example::Application.config.session_store :cookie_store, key: '_heroku-direct-s3-example_session'
